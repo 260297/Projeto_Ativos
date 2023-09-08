e45cf168e6abd0e45ec387f20e5b175d0144faca
260297 <robertomanica@yahoo.com.br>
2023-09-08 16:10:50 -0300

    debug

--- ativos/settings.py
+++ ativos/settings.py
@@ -125,6 +125,7 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATIC_ROOT= os.path.join(BASE_DIR, 'static')
 
 
 
